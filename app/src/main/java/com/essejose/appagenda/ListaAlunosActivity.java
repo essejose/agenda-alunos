f716336f4c54b0273fbca2e7fd145f72accfad66
essejose <ze_pk2@hotmail.com>
2017-05-23 23:39:54 -0300

    first commit

---
+++ app/src/main/java/com/essejose/appagenda/ListaAlunosActivity.java
@@ -0,0 +1,19 @@
+package com.essejose.appagenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Jose","Ronaldo","Antonio","Felipe"};
+
+        ListView listView
+
+    }
+}
